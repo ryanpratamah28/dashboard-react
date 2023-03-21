16df1c6a6cc30d88a2827df00833904085726eb4
RyanPrata01 <nayr.saena212@gmail.com>
2023-03-21 10:53:19 +0700

    fetch(app): Adding new feature

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import {
-  CContainer,
-  CHeader,
-  CHeaderBrand,
-  CDropdown,
-  CDropdownToggle,
-  CDropdownMenu,
-  CDropdownItem,
-  CHeaderNav,
-  CHeaderToggler,
-  CNavLink,
-  CNavItem,
+    CContainer,
+    CHeader,
+    CHeaderBrand,
+    CHeaderNav,
+    CHeaderToggler,
+    CNavLink,
+    CNavItem,
+    CButton,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilMenu } from '@coreui/icons'
@@ -25,70 +22,112 @@ import IndonesianFlag from '../../src/assets/images/avatars/Indonesian.jpg'
 import EnglandFlag from '../../src/assets/images/avatars/England.png'
 
 const AppHeader = () => {
-  const dispatch = useDispatch()
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+    const dispatch = useDispatch()
+    const sidebarShow = useSelector((state) => state.sidebarShow)
 
-  return (
-    <CHeader
-      className=""
-      style={{
-        marginBottom: '40px',
-        backgroundColor: 'transparent',
-        border: 'none',
-      }}
-    >
-      <CContainer fluid style={{ marginBottom: '30px' }}>
-        <CHeaderToggler
-          className="ps-1"
-          onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
+    const [language, setLanguage] = useState('Id')
+    const toggleLanguage = () => {
+        setLanguage(language === 'Id' ? 'En' : 'Id')
+    }
+
+    return (
+        <CHeader
+            className=""
+            style={{
+                marginBottom: '40px',
+                backgroundColor: 'transparent',
+                border: 'none',
+            }}
         >
-          <CIcon icon={cilMenu} size="lg" style={{ color: 'white' }} />
-        </CHeaderToggler>
+            <CContainer fluid style={{ marginBottom: '30px' }}>
+                <CHeaderToggler
+                    className="ps-1"
+                    onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
+                >
+                    <CIcon icon={cilMenu} size="lg" style={{ color: 'white' }} />
+                </CHeaderToggler>
 
-        <CHeaderBrand className="mx-auto d-md-none" to="/">
-          <CIcon icon={logo} height={48} alt="Logo" />
-        </CHeaderBrand>
+                <CHeaderBrand className="mx-auto d-md-none" to="/">
+                    <CIcon icon={logo} height={48} alt="Logo" />
+                </CHeaderBrand>
 
-        <CHeaderNav className="ms-3">
-          <CDropdown>
-            <CDropdownToggle color="light" variant="outline" className="IndonesianFlag">
-              {' '}
-              <img src={IndonesianFlag} style={{ width: '25px' }}></img> ID
-            </CDropdownToggle>
-            <CDropdownMenu className="mt-1">
-              <CDropdownItem href="#">
-                <img src={EnglandFlag} style={{ width: '25px' }}></img> EN
-              </CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
+                <CHeaderNav className="ms-3">
+                    <div className="me-2 bg-primary p-1 rounded-pill">
+                        <div className="btn-language d-flex">
+                            <CButton
+                                onClick={toggleLanguage}
+                                id="Id"
+                                style={{ marginRight: '5px' }}
+                                className={
+                                    language === 'Id'
+                                        ? 'd-flex bg-light border-0 rounded-pill text-dark active me-2 transition-all duration-500'
+                                        : 'd-flex bg-primary border-0 rounded-pill text-light me-2'
+                                }
+                            >
+                                <img
+                                    src={IndonesianFlag}
+                                    width="25"
+                                    className="d-flex justify-content-center align-items-center me-2 rounded-1"
+                                ></img>
+                                <h6
+                                    className="d-flex justify-content-center align-items-center"
+                                    style={{ margin: 0 }}
+                                >
+                                    ID
+                                </h6>
+                            </CButton>
 
-          <AppHeaderDropdown />
-        </CHeaderNav>
-      </CContainer>
+                            <CButton
+                                onClick={toggleLanguage}
+                                id="En"
+                                className={
+                                    language === 'En'
+                                        ? 'd-flex bg-light border-0 rounded-pill text-dark active'
+                                        : 'd-flex bg-primary border-0 rounded-pill text-light'
+                                }
+                            >
+                                <img
+                                    src={EnglandFlag}
+                                    width="30"
+                                    className="d-flex justify-content-center align-items-center me-2 rounded-1"
+                                ></img>
+                                <h6
+                                    className="d-flex justify-content-center align-items-center"
+                                    style={{ margin: 0 }}
+                                >
+                                    EN
+                                </h6>
+                            </CButton>
+                        </div>
+                    </div>
 
-      <CContainer fluid className="d-none d-md-flex me-auto">
-        <CHeaderNav>
-          <CNavItem>
-            <CNavLink to="/dashboard" component={NavLink} style={{ color: 'white' }}>
-              Dashboard
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#" style={{ color: 'white' }}>
-              Users
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#" style={{ color: 'white' }}>
-              Settings
-            </CNavLink>
-          </CNavItem>
-        </CHeaderNav>
+                    <AppHeaderDropdown />
+                </CHeaderNav>
+            </CContainer>
 
-        <AppBreadcrumb />
-      </CContainer>
-    </CHeader>
-  )
+            <CContainer fluid className="d-none d-md-flex me-auto">
+                <CHeaderNav>
+                    <CNavItem>
+                        <CNavLink to="/dashboard" component={NavLink} style={{ color: 'white' }}>
+                            Dashboard
+                        </CNavLink>
+                    </CNavItem>
+                    <CNavItem>
+                        <CNavLink href="#" style={{ color: 'white' }}>
+                            Users
+                        </CNavLink>
+                    </CNavItem>
+                    <CNavItem>
+                        <CNavLink href="#" style={{ color: 'white' }}>
+                            Settings
+                        </CNavLink>
+                    </CNavItem>
+                </CHeaderNav>
+
+                <AppBreadcrumb />
+            </CContainer>
+        </CHeader>
+    )
 }
 
 export default AppHeader
